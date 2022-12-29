90e10445065cd5f7cc3c69cf139a9278e94c351d
Katherine Schuljak <kschuljak@gmail.com>
2022-12-29 10:46:56 -0800

    font and background added

---
+++ tenmo-client/src/main/java/com/techelevator/tenmo/views/Colors.java
@@ -0,0 +1,24 @@
+package com.techelevator.tenmo.views;
+
+public class Colors {
+
+    public static final String RESET = "\u001B[0m";
+
+    public static final String BLACK_FONT = "\u001B[30m";
+    public static final String RED_FONT = "\u001B[31m";
+    public static final String GREEN_FONT = "\u001B[32m";
+    public static final String YELLOW_FONT = "\u001B[33m";
+    public static final String BLUE_FONT = "\u001B[34m";
+    public static final String PURPLE_FONT = "\u001B[35m";
+    public static final String CYAN_FONT = "\u001B[36m";
+    public static final String WHITE_FONT = "\u001B[37m";
+
+    public static final String BLACK_BACKGROUND = "\u001B[40m";
+    public static final String RED_BACKGROUND = "\u001B[41m";
+    public static final String GREEN_BACKGROUND = "\u001B[42m";
+    public static final String YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String BLUE_BACKGROUND = "\u001B[44m";
+    public static final String PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String CYAN_BACKGROUND = "\u001B[46m";
+    public static final String WHITE_BACKGROUND = "\u001B[47m";
+}
